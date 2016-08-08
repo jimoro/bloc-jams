0689fefc845122665f20034b7718eb161d1a324c
Jim Rohrer <jim.rohrer@gmail.com>
2016-08-08 10:36:48 -0700

    For discussion - Added missing landing.js and index.html

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,6 +1,22 @@
 var pointsArray = document.getElementsByClassName('point');
 
 var animatePoints = function(points) {
+    var revealPoint = forEach(points, function() {
+        this.points.style.opacity = 1;
+        this.points.style.transform = "scaleX(1) translateY(0)";
+        this.points.style.msTransform = "scaleX(1) translateY(0)";
+        this.points.style.WebkitTransform = "scaleX(1) translateY(0)";
+    });
+    revealPoint();
+};
+
+
+
+/* Working code (with for loop to be replaced by forEach function) 
+
+var pointsArray = document.getElementsByClassName('point');
+
+var animatePoints = function(points) {
     for (i = 0; i < points.length; i++) {
         var revealPoint = function(i) {
             points[i].style.opacity = 1;
@@ -11,6 +27,8 @@ var animatePoints = function(points) {
         revealPoint(i);
     }
 };
+*/
+
 
 window.onload = function() {
         // Automatically animate the points on a tall screen where scrolling can't trigger the animation
